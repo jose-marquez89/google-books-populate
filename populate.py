9ecffab0adc4c144004099620931484cc35926d9
jose-marquez89 <jlmarquez89@gmail.com>
2020-05-01 18:22:35 -0500

    adds url construction and query execute functions

--- populate.py
+++ populate.py
@@ -1,6 +1,8 @@
 import requests
 import logging
 import os
+import threading
+from urllib.parse import urljoin, quote
 
 import psycopg2
 from psycopg2 import sql
@@ -8,20 +10,47 @@ from psycopg2 import sql
 FORMAT = "%(asctime)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
-api_url = "https://www.googleapis.com/books/v1/volumes?q="
-publisher_param = "inpublisher:"
-publisher = 'penguin'
-sep = "&"
-max_results = "maxResults=40"
-res = requests.get(api_url + publisher_param +
-                   publisher + sep + max_results)
-book_data = res.json()
-books = book_data['items']
-values = []
+# books = book_data['items']
+# values = []
+
+
+def gb_url(search_term, index=None):
+    """
+    Creates a valid url for request
+
+    search_term: the terms to search for via the specified parameter
+
+    index: defaults to None, can be used to paginate results
+    """
+    base_url = "https://www.googleapis.com/books/v1/"
+    volumes = "volumes?q="
+    parameter = "inpublisher:"
+    max_results = "&maxResults=40"
+
+    if index:
+        tail = (volumes +
+                parameter +
+                quote(search_term) +
+                max_results +
+                f"&startIndex={index}")
+    else:
+        tail = (volumes +
+                parameter +
+                quote(search_term) +
+                max_results)
+
+    url = urljoin(base_url, tail)
+
+    return url
 
 
 def get_value(book):
+    """
+    Compiles book data from json response into an iterable
+    for use in SQL command
 
+    book: one individual item in googleAPIresponse['items']
+    """
     try:
         googleId = book['id']
     except KeyError:
@@ -119,7 +148,7 @@ def get_value(book):
     except KeyError:
         subtitle = None
 
-    value = [googleId, title, authors, publisher, publishedDate,
+    value = [googleId, title, authors, pub, publishedDate,
              description, isbn, pageCount, categories, thumbnail,
              smallThumbnail, lang, webReaderLink, textSnippet,
              isEbook, averageRating, maturityRating, ratingsCount,
@@ -128,9 +157,74 @@ def get_value(book):
     return value
 
 
-for book in books:
-    value = get_value(book)
-    values.append(value)
+def request_and_execute(search_term, starting_index):
+    """
+    Creates a GET request to google books API, collects data, then uses
+    this data to execute a SQL query with connection to database
+    """
+
+    # create connection
+    DATABASE_URL = os.environ["DATABASE_URL"]
+
+    connection = psycopg2.connect(DATABASE_URL)
+    cursor = connection.cursor()
+
+    # GET request
+    url = gb_url(search_term, index=starting_index)
+    response = requests.get(url)
+
+    try:
+        response.raise_for_status()
+    except Exception as err:
+        logging.error(err)
+
+        return None
+
+    data = response.json()
+
+    if 'items' not in data.keys():
+        logging.info(
+            f"No items for {search_term} at index {starting_index}"
+        )
+
+        return None
+
+    books = data['items']
+    values = []
+
+    for book in books:
+        values.append(get_value(book))
+
+    for entry in values:
+        query = sql.SQL(
+            "INSERT INTO gb_test VALUES "
+            "(%s, %s, %s, %s, %s, %s, %s, "
+            "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        )
+        try:
+            cursor.execute(query, entry)
+        except Exception as err:
+            logging.error(f"Error: {err}")
+            connection.rollback()
+        else:
+            connection.commit()
+
+    cursor.close()
+    connection.close()
+
+    return True
+
+
+def get_publisher_books():
+
+    """Gets books data from index 0 of API request pages, starts
+    necessary threads to collect other pages
+    """
+    pass
+
+# for book in books:
+    # value = get_value(book)
+    # values.append(value)
 
 
 # TODO: make a call to the api for the maximum allowable results - 40
@@ -143,10 +237,10 @@ for book in books:
 
 # TODO: query the database; add book to database (rate limit)
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+# DATABASE_URL = os.environ["DATABASE_URL"]
 
-connection = psycopg2.connect(DATABASE_URL)
-cursor = connection.cursor()
+# connection = psycopg2.connect(DATABASE_URL)
+# cursor = connection.cursor()
 
 if __name__ == "__main__":
     for entry in values:
